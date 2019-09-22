ee7d2ad7f55a1c5cd90020bb431048553e6149f6
Merlin Webster <mwebster@witekio.com>
2019-09-22 08:29:22 +0100

    Command: Added an example CLI based on Command pattern

--- command/command.py
+++ command/command.py
@@ -1,8 +1,12 @@
+import sys
+
+# Command interface
 class Command:
     def execute(self):
         raise NotImplementedError
 
 
+# Concerete Command implimenting Command interface
 class LogCommand(Command):
     def __init__(self, reciever):
         self.reciever = reciever
@@ -11,21 +15,54 @@ class LogCommand(Command):
         self.reciever.log()
 
 
+
+class InvalidCommandError(Exception):
+    pass
+
+
 class Invoker:
     def __init__(self):
-        self.command = None
+        self.commands = {}
 
-    def set_command(self, command):
-        self.command = command
+    def set_command(self, name, command):
+        if not isinstance(command, Command):
+            raise AttributeError('command must be a Command class')
+
+        self.commands[name] = command
+
+    def run_command(self, name):
+        if name not in self.commands:
+            raise InvalidCommandError(f'{name}: command not found')
+
+        self.commands[name].execute()
+
+
+class ExitCommand(Command):
+    def execute(self):
+        sys.exit()
+
+
+class CommandLineInterface(Invoker):
+    def __init__(self):
+        Invoker.__init__(self)
+
+        self.set_command('exit', ExitCommand())
 
     def run(self):
-        self.command.execute()
+        while True:
+            try:
+                cmd = input('cli$ ')
+                self.run_command(cmd)
+            except EOFError:
+                self.run_command('exit')
+            except InvalidCommandError as e:
+                print(str(e))
 
 
+# Reciever
 class Logger:
     def __init__(self, message):
         self.message = message
 
     def log(self):
         print(self.message)
-
